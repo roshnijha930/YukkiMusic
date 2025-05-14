173af4577faf9ad4f92940026296888d2e36c4a6
roshnijha930 <roshnijha930@gmail.com>
2025-05-14 17:58:48 +0530

    Update YukkiMusic.py

--- YukkiMusic.py
+++ YukkiMusic.py
@@ -49,13 +49,13 @@ logs = logging.getLogger()
 if os.path.exists("config.env"):
     load_dotenv("config.env")
 
-API_ID = int(getenv("API_ID", 25742938))
+API_ID = int(getenv("API_ID", 22163320))
 API_HASH = getenv("API_HASH", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7009601543").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6317447505").split()))
 OWNER_ID = int(getenv("OWNER_ID", 0))
 START_IMAGE_URL = getenv("START_IMAGE_URL", None)
 
